5a58589ce6b81d59553400fa05b13b1d335ee48c
Fahim-BAUST <ashhabf50@gmail.com>
2021-11-04 16:17:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+          <Footer></Footer>
+        </Switch>
+
+
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="sticky-top shadow ">
+            <nav className="navbar bg-secondary bg-opacity-10 navbar-expand-lg navbar-light  text-dark ">
+                <div className="container-fluid">
+                    <NavLink
+                        className="navbar-brand "
+                        to="/home"
+                    >
+                        <img style={{ width: "130px", height: "40px" }} className="img-fluid" src="https://i.ibb.co/cyRQvCT/coollogo-com-269541991.gif" alt="" />
+                    </NavLink>
+                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link "
+                                    to="/home"
+                                    activeStyle={{
+                                        fontWeight: "bolder"
+                                    }}
+                                >
+                                    <i className="fas fa-home"></i>   Home
+                                </NavLink>
+
+                            </li>
+                            <li className="nav-item">
+
+                                <NavLink
+                                    className="nav-link "
+                                    to="/Books"
+
+                                    activeStyle={{
+                                        fontWeight: "bolder"
+
+                                    }}
+                                >
+                                    <i className="fas fa-book"></i>   Books
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+
+                                <NavLink
+                                    className="nav-link "
+                                    to="/about"
+
+                                    activeStyle={{
+                                        fontWeight: "bolder"
+                                    }}
+                                >
+                                    <i className="fas fa-users"></i> About us
+                                </NavLink>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <NavLink
+                                    activeStyle={{
+                                        fontWeight: "bolder"
+                                    }}
+                                    to="" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fas fa-tasks"></i> Check Orders
+                                </NavLink>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <NavLink
+                                            className="nav-link text-dark dropdown-item"
+                                            to="/myOrder"
+
+                                            activeStyle={{
+                                                fontWeight: "bolder",
+
+                                            }}
+                                        >
+                                            My order
+                                        </NavLink>
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <NavLink
+                                    activeStyle={{
+                                        fontWeight: "bolder"
+                                    }}
+                                    to="" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fas fa-tasks"></i> Manage
+                                </NavLink>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <NavLink
+                                            className="nav-link text-dark dropdown-item"
+                                            to="/allOrder"
+                                            activeStyle={{
+                                                fontWeight: "bolder"
+
+                                            }}
+                                        >
+                                            All order
+                                        </NavLink>
+
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            className="nav-link text-dark dropdown-item"
+                                            to="/addBook"
+                                            activeStyle={{
+                                                fontWeight: "bolder"
+
+                                            }}
+                                        >
+                                            Add Book
+                                        </NavLink>
+
+
+                                    </li>
+
+                                </ul>
+                            </li>
+                        </ul>
+                        <form className="d-flex align-items-center">
+
+                            <NavLink
+                                className="nav-link text-warning "
+                                to="/cart"
+                            >
+                                <i className="fas fa-cart-arrow-down fs-2"></i>
+                            </NavLink>
+
+                            {/* <img style={{ width: "35px" }} className="img-fluid rounded-circle" src='' alt="" /> */}
+                            <NavLink to="/login"><button className="btn btn-outline-info" type="submit"><i className="fas fa-sign-in-alt"></i> Login</button></NavLink>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default Header;
