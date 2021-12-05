c254d9e5ad3c0471733ae666f6b267f4504f227b
Fahim-BAUST <ashhabf50@gmail.com>
2021-12-05 19:23:47 +0600

    update

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -7,25 +7,6 @@ import Font from 'react-font';
 
 
 const Home = () => {
-    const [novels, setNovels] = useState([]);
-    const [educations, setEducations] = useState([]);
-    const [islamic, setIslamic] = useState([]);
-    const url = './book.json';
-
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-
-                const novels = data.filter(data => data.subCatagory === 'uponnash')
-                setNovels(novels);
-                const educations = data.filter(data => data.subCatagory === 'education')
-                setEducations(educations);
-                const islamic = data.filter(data => data.subCatagory === 'islamic')
-                setIslamic(islamic);
-            })
-
-    }, [])
 
     return (
         <div style={{ backgroundColor: "#f1f2f4", marginBottom: "15px", paddingBottom: 20 }}>
