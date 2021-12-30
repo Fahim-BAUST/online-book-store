84f9161a09184cb17cdb19cdc94914d35e4a2611
Anik mahmud <96568968+AnikMahmud78@users.noreply.github.com>
2021-12-31 00:20:04 +0600

    products

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Banner from '../Banner/Banner';
 import './Home.css'
 import Font from 'react-font';
+import Products from '../Products/Products';
 
 
 const Home = () => {
@@ -11,7 +12,7 @@ const Home = () => {
     return (
         <div style={{ backgroundColor: "#f1f2f4", marginBottom: "15px", paddingBottom: 20 }}>
             <Banner></Banner>
-
+            <Products></Products>
 
         </div>
     );
---
+++ src/Pages/Home/Products/Products.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import Rating from 'react-rating';
+import './products.css';
+const Products = () => {
+    // fetching(getting) books from json file 
+    // aiekhane json file theke book gulo k fetch kore nie asha hoice and 'products' a set kora hoice .
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('/book.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
+    }, []);
+    return (
+        <div className="text-center">
+        <h1 className='pt-5'>Our Products</h1>
+        {/* <h4 style={{ textAlign: "start", padding: "10px" }}></h4> */}
+        {/* card gulo row r column use kore dekhano hocce(small device,medium device and learge device)  */}
+        <div className=' row row-cols-1 row-cols-md-2 row-cols-lg-5 g-4 m-0 container-fluid'>
+
+            {
+                products?.slice(0, 5).map(product => <div
+                    key={product?.key}
+                    className='customcard shadow'
+                >    <img style={{ height: "200px" }} className='w-50%' src={product?.image} alt="" />
+                    <h5 style={{ textAlign: "start" }}>{product?.bookName.slice(0, 20)}...</h5>
+                    <p style={{ textAlign: "start" }}>{product?.details.slice(0, 30)}...</p>
+                    <p style={{ textAlign: "start",color:"rgba(255, 127, 80, 0.89)" }}>
+                        <small ><Rating
+                            initialRating={product?.rating}
+                            emptySymbol="far fa-star"
+                            fullSymbol="fas fa-star"
+                            readonly></Rating></small>
+                    </p>
+                    <p style={{ textAlign: "start" }}>৳ {product?.cost}</p>
+                    {/* <Link to={`/placeOrder/${product?.id}`}> */}
+                        <button>Buy Now</button>
+                    {/* </Link> */}
+                </div>)
+            }
+        </div>
+        <h4 style={{ textAlign: "start", padding: "20px" }}></h4>
+        <div className=' row row-cols-1 row-cols-md-2 row-cols-lg-5 g-4 m-0 container-fluid'>
+
+        {
+                products?.slice(5, 10).map(product => <div
+                    key={product?.key}
+                    className='customcard shadow'
+                >    <img style={{ height: "200px" }} className='w-50%' src={product?.image} alt="" />
+                    <h5 style={{ textAlign: "start" }}>{product?.bookName.slice(0, 20)}...</h5>
+                    <p style={{ textAlign: "start" }}>{product?.details.slice(0, 30)}...</p>
+                    <p style={{ textAlign: "start",color:"rgba(255, 127, 80, 0.89)" }}>
+                        <small ><Rating
+                            initialRating={product?.rating}
+                            emptySymbol="far fa-star"
+                            fullSymbol="fas fa-star"
+                            readonly></Rating></small>
+                    </p>
+                    <p style={{ textAlign: "start" }}>৳ {product?.cost}</p>
+                    {/* <Link to={`/placeOrder/${product?.id}`}> */}
+                        <button>Buy Now</button>
+                    {/* </Link> */}
+                </div>)
+            }
+        </div>
+    </div>
+    );
+};
+
+export default Products;
