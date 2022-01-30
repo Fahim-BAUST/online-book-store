8de518ed8910872f760fb7c1708f3e01fe545faf
FahimAshhab <f.ashhab@shiftboolean.com>
2022-01-30 16:13:35 +0600

    login,registration,search

---
+++ src/Pages/Authenticarion/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbUnJcibHd_7_NO85CsbfvLijb71FTn24",
+    authDomain: "bookstore-9683d.firebaseapp.com",
+    projectId: "bookstore-9683d",
+    storageBucket: "bookstore-9683d.appspot.com",
+    messagingSenderId: "1095194612739",
+    appId: "1:1095194612739:web:8e082f222a512c8049ea36",
+    measurementId: "G-NDKZ9EC1TW"
+};
+export default firebaseConfig;
--- src/Pages/Books/Books.js
+++ src/Pages/Books/Books.js
@@ -1,27 +1,44 @@
 import { Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Book from './Book/Book';
 
 const Books = () => {
     const [books, setBooks] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
 
     useEffect(() => {
 
-        fetch('./book.json')
+        fetch('http://localhost:5000/books')
             .then(res => res.json())
-            .then(data => setBooks(data))
+            .then(data => {
+                setBooks(data)
+                setDisplayProducts(data)
+            })
     }, [])
+
+    const handleSearch = event => {
+        const searchText = event.target.value;
+
+        const matchedProducts = books.filter(product => product.bookName.toLowerCase().includes(searchText.toLowerCase()));
+
+        setDisplayProducts(matchedProducts);
+    }
     return (
         <div>
             <Typography className="service-name" sx={{ textAlign: "center", pt: 3, fontWeight: 800, marginTop: 2, marginBottom: 5, color: "#03e12", fontSize: "60px", fontFamily: "cursive" }} variant="h3">All Books</Typography>
+
+            <div className="input-group mb-4 container search-item ">
+                <input type="text" className="form-control shadow-lg text-center" onChange={handleSearch}
+                    placeholder="Search Product" aria-label="Recipient's username" aria-describedby="basic-addon2" style={{ border: "1px solid #3F000F" }} />
+            </div>
+
             <Box className="container" sx={{ flexGrow: 1 }}>
                 <Grid container spacing={{ xs: 2, md: 6 }} columns={{ xs: 3, sm: 8, md: 12 }}>
                     {
-                        books.map(book => <Grid
+                        displayProducts.map(book => <Grid
                             className="grid-responsive" item xs={12} sm={4} md={4} >
                             <Book book={book}></Book>
                         </Grid>)
