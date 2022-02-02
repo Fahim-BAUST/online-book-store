0f42f7675862d60c8b2a843240836d1ffd57e257
FahimAshhab <f.ashhab@shiftboolean.com>
2022-02-02 22:23:00 +0600

    123

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
 
                 <h1 className="mt-4 pt-4 mb-5 fw-bold text-center site-name animate__animated animate__backInDown" style={{
                     color: "#25383C", fontSize: "60px"
-                }}>BookStore 103</h1></Font>
+                }}>BookStore</h1></Font>
 
             {/* ekhane bootsrap er carrousol thekeniye edit kora  */}
 
