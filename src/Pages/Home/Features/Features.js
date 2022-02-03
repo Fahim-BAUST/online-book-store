e8659dc1dffccca66e96aa336ed7845c0309619f
FahimAshhab <f.ashhab@shiftboolean.com>
2022-02-03 22:29:51 +0600

    done

--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import './Features.css';
-import img1 from './img/asdade.PNG'
-import img2 from './img/100e.PNG'
-import img3 from './img/upcomming.PNG'
-import img4 from './img/books.PNG'
-import img5 from './img/author.PNG'
+import img1 from './img/awdwad.PNG'
+import img2 from './img/awdad.PNG'
+import img3 from './img/ertrgv.PNG'
+import img4 from './img/dawdawd.PNG'
+
 const Features = () => {
     return (
         <div className='pt-5' >
-            <h3 className='text-center'>Features</h3>
+            
             <div className='container-fluid shadow '>
             <div className="row ">
                 <div className="col text-center">
                 <img className='pt-3' src={img1} alt="" />
-                <p>Customer Reviews</p>
+                <p>Free Delivery</p>
                 </div>
                 <div className="col text-center">
                 <img className='pt-4' src={img2} alt="" />
-                <p>100 Must Reads</p>
+                <p>5 Days Return</p>
                 </div>
                 <div className="col text-center">
                 <img className='pt-3' src={img3} alt="" />
-                <p>Upcoming Releases</p>
+                <p>Secure Payment</p>
                 </div>
                 <div className="col text-center">
                 <img className='pt-4' src={img4} alt="" />
-                <p>Textbooks</p>
+                <p>24/7 Support</p>
                 </div>
-                <div className="col text-center">
-                <img className='pt-3' src={img5} alt="" />
-                <p>Author Recommended</p>
-                </div>
+                
             </div>
             </div>
         </div>
